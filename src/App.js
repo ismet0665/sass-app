1eb538aa65efa2b83686b7490a971bf762b405e2
ismet0665 <ismet.akin@gmail.com>
2023-02-26 00:21:52 +0300

    sass end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import "./scss/app.scss"; // klasik css gibi import ediyoruz. module.css olsaydı from lu şekilde import edecektik.
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Card data={data} />
+      <Footer />
+    </>
   );
 }
 
